7e539a860ff36971b50ea55baca00b4271e8f418
Mustafa <120393372+droptablemustafa@users.noreply.github.com>
2025-11-14 22:18:56 +0000

    add: soundcloud webview

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow } from "electron";
+
+const createWindow = () => {
+	const win = new BrowserWindow({
+		width: 1200,
+		height: 800,
+		webPreferences: {
+			nodeIntegration: false,
+			contextIsolation: true,
+			webviewTag: true,
+		},
+	});
+
+	win.loadFile("src/index.html");
+};
+
+app.whenReady().then(() => {
+	createWindow();
+
+	app.on("activate", () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
+});
+
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
